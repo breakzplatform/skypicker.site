da3d9f8d63766c956e1aa7c2f266e6b630bcff1a
Joselito <eu@joseli.to>
2023-07-04 21:14:45 -0300

    emoji favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
