80444bc7150b25f7d175f7ce4a7871a8a1c61698
Joselito <eu@joseli.to>
2024-09-10 01:34:08 -0300

    fix button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -48,7 +48,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-        onClick={props.click ? props.click : null}
       />
     )
   }
